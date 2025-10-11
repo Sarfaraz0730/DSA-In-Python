95de93ac6a004dbefb2c5fd27e3d13c7c21bd1b7
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2025-10-11 23:10:40 +0530

    for loop

---
+++ Loops/forLoop.py
@@ -0,0 +1,19 @@
+
+# n = int(input("Enter a number: " ))
+# sum = 0
+# for i in range(n):
+ 
+#     print("Hello ", i)
+#     sum +=i +1
+#     print("sum till now is: ", sum)
+
+
+
+# print("Final sum is : ", sum)
+
+
+arr =[2,4,6]
+sum=0
+for i in arr:
+    sum +=i
+print("Sum is : ", sum)
