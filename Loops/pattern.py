97c0067b54c194452aee229ee5e8786606823d78
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2025-10-14 23:00:11 +0530

    loop practice

--- Loops/pattern.py
+++ Loops/pattern.py
@@ -1,10 +1,46 @@
 
-n = int(input("Enter a count of rows: "))
-m = int(input("Enter a count of columns: "))
+# n = int(input("Enter a count of rows: "))
+# m = int(input("Enter a count of columns: "))
 
 
-for i in range(1,n+1):
-    for j in range(1,m+1):
-        print(i,j)
+# for i in range(1,n+1):
+#     for j in range(1,m+1):
+#         print(i,j)
 
-    print()
+#     print()
+
+
+# Print numbers from 1 to 100 
+
+# for i in range(1,101):
+#     print(i)
+
+
+# Print even and odd numbers from 1–50 
+
+# for i in range(1, 51):
+#     if i%2==0:
+#         print(i, "is even number")
+#     else:
+#         print(i, "is odd number")
+
+
+# Find the sum of numbers from 1–N 
+
+# n = int(input("Enter a number: "));
+
+# sum = 0
+
+# for i in range (1, n+1):
+#     sum +=i
+
+# print("Sum is: ", sum)
+
+
+# Print multiplication table of any number 
+
+
+# n = int(input("Enter a number: "));
+
+# for i in range(1, 11):
+#     print(n, "X", i , "=", n*i)
